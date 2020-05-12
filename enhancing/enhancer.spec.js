e38d617171df1b73c2bde119d1239048da89ec0a
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-11 19:36:32 -0600

    Added empty tests for each of the functions we will be testing.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,20 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("enhancer unit tests", () => {
+    it("succeeds", () => {
+        expect(enhancer.succeed().toBe())
+    })
+
+    it("fails", () => {
+        expect(enhancer.fail().toBe())
+    })
+
+    it("repairs", () => {
+        expect(enhancer.repair().toBe())
+    })
+
+    it("gets", () => {
+        expect(enhancer.get().toBe())
+    })
+})
