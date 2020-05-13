ee511d98e68b85650919853d588d9e0e305d9a48
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-12 19:12:29 -0600

    Fixed tests for fail() and added another item to test.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,25 +13,27 @@ function succeed(item) {
   }
 }
 
+
 function fail(item) {
-  if (item.enhancement < 15) {
-    return { ...item, durability: item.durability - 5 }
+  if (item.enhancement < 15 && item.durability < 5 ) {
+    return {...item, durability: 0}
+  } else if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5}
+  } else if ( item.enhancement >= 15 && item.enhancement > 16 && item.durability < 10) {
+  return { ...item, durability: 0, enhancement: item.enhancement -1 }
+  } else if (item.enhancement >= 15 && item.enhancement > 16 ) {
+    return {...item, durability: item.durability - 10, enhancement: item.enhancement - 1 }
   } else if ( item.enhancement >= 15 ) {
-  return { ...item, durability: item.durability - 10 }
-  }
-
-  if (item.enhancement > 16 ) {
-  return { ...item, enhancement: item.enhancement - 1 }
-  }
-
-  if (item.durability < 5 ) {
-  return { ...item, durability: 0 }
+    return { ...item, durability: item.durability - 10 }
   }
 }
+
 
 function repair(item) {
   return { ...item, durability: 100 };
 }
+
+
 
 function get(item) {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -34,57 +34,68 @@ describe("enhancer unit tests", () => {
 ///////////// FAILS /////////////
 
 
-// it("fails", () => {
-//     expect(enhancer.fail({
-//         name: "Car",
-//         durability: 20,
-//         enhancement: 2
-//     })).toEqual({
-//         name: "Car",
-//         durability: 15,
-//         enhancement: 2      
-//     })
+it("fails", () => {
+    expect(enhancer.fail({
+        name: "Car",
+        durability: 20,
+        enhancement: 2
+    })).toEqual({
+        name: "Car",
+        durability: 15,
+        enhancement: 2      
+    })
 
-// })
+})
     
-// it("fails", () => {
-//     expect(enhancer.fail({
-//         name: "Plane",
-//         durability: 20,
-//         enhancement: 20
-//     })).toEqual({
-//         name: "Plane",
-//         durability: 10,
-//         enhancement: 19      
-//     })
+it("fails", () => {
+    expect(enhancer.fail({
+        name: "Plane",
+        durability: 20,
+        enhancement: 20
+    })).toEqual({
+        name: "Plane",
+        durability: 10,
+        enhancement: 19      
+    })
 
-// })
+})
 
-// it("fails", () => {
-//     expect(enhancer.fail({
-//         name: "Train",
-//         durability: 4,
-//         enhancement: 20
-//     })).toEqual({
-//         name: "Train",
-//         durability: 0,
-//         enhancement: 19      
-//     })
+it("fails", () => {
+    expect(enhancer.fail({
+        name: "Train",
+        durability: 4,
+        enhancement: 20
+    })).toEqual({
+        name: "Train",
+        durability: 0,
+        enhancement: 19      
+    })
 
-// })
+})
 
-// it("fails", () => {
-//     expect(enhancer.fail({
-//         name: "Bike",
-//         durability: 4,
-//         enhancement: 10
-//     })).toEqual({
-//         name: "Bike",
-//         durability: 0,
-//         enhancement: 10      
-//     })
+it("fails", () => {
+    expect(enhancer.fail({
+        name: "Bike",
+        durability: 4,
+        enhancement: 10
+    })).toEqual({
+        name: "Bike",
+        durability: 0,
+        enhancement: 10      
+    })
+})
 
-// })
+it("fails", () => {
+    expect(enhancer.fail({
+        name: "Scooter",
+        durability: 9,
+        enhancement: 20
+    })).toEqual({
+        name: "Scooter",
+        durability: 0,
+        enhancement: 19      
+    })
+})
 
 
 ///////////// REPAIRS /////////////
