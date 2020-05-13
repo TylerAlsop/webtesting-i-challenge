554d6cc29e452ff6ae8901f05cd74ccd36d10f60
Tyler Alsop <tylerjalsop@gmail.com>
2020-05-12 19:01:26 -0600

    Attempted the fail() testing, but 3 of the tests are not passing.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -34,9 +34,57 @@ describe("enhancer unit tests", () => {
 ///////////// FAILS /////////////
 
 
-    // it("fails", () => {
-    //     expect(enhancer.fail().toBe())
-    // })
+// it("fails", () => {
+//     expect(enhancer.fail({
+//         name: "Car",
+//         durability: 20,
+//         enhancement: 2
+//     })).toEqual({
+//         name: "Car",
+//         durability: 15,
+//         enhancement: 2      
+//     })
+
+// })
+    
+// it("fails", () => {
+//     expect(enhancer.fail({
+//         name: "Plane",
+//         durability: 20,
+//         enhancement: 20
+//     })).toEqual({
+//         name: "Plane",
+//         durability: 10,
+//         enhancement: 19      
+//     })
+
+// })
+
+// it("fails", () => {
+//     expect(enhancer.fail({
+//         name: "Train",
+//         durability: 4,
+//         enhancement: 20
+//     })).toEqual({
+//         name: "Train",
+//         durability: 0,
+//         enhancement: 19      
+//     })
+
+// })
+
+// it("fails", () => {
+//     expect(enhancer.fail({
+//         name: "Bike",
+//         durability: 4,
+//         enhancement: 10
+//     })).toEqual({
+//         name: "Bike",
+//         durability: 0,
+//         enhancement: 10      
+//     })
+
+// })
 
 
 ///////////// REPAIRS /////////////
